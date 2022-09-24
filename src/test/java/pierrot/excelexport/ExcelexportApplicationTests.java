02308ad281d06501f2037361b7716f23a91b9452
PierreSQS <nomo_messina@web.de>
2022-09-24 18:07:08 +0200

    commit02: added assertion to integrations test

--- src/test/java/pierrot/excelexport/ExcelexportApplicationTests.java
+++ src/test/java/pierrot/excelexport/ExcelexportApplicationTests.java
@@ -2,12 +2,16 @@ package pierrot.excelexport;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ExcelexportApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext appCtx) {
+		assertThat(appCtx).isNotNull();
 	}
 
 }
