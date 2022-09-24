52e1cbda571a870f6349a97633cd80545780ca23
PierreSQS <nomo_messina@web.de>
2022-09-24 18:04:07 +0200

    commit01: initialzr commit

---
+++ src/test/java/pierrot/excelexport/ExcelexportApplicationTests.java
@@ -0,0 +1,13 @@
+package pierrot.excelexport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelexportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
