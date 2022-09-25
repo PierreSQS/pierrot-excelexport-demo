ec8098fcd3f8489c9f5ed875debdae05cf3467a4
PierreSQS <nomo_messina@web.de>
2022-09-25 07:34:16 +0200

    commit08: added an assertion on View name

--- src/test/java/pierrot/excelexport/controller/ExcelExportControllerTest.java
+++ src/test/java/pierrot/excelexport/controller/ExcelExportControllerTest.java
@@ -8,9 +8,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import pierrot.excelexport.excel.ExcelView;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(ExcelExportController.class)
 class ExcelExportControllerTest {
@@ -29,7 +32,9 @@ class ExcelExportControllerTest {
     void downloadExcel() throws Exception {
         mockMvc.perform(get("/excel/download"))
                 .andExpect(status().isOk())
+//May produce NPE. lived in code for documentation purposes!!!
 //                .andExpect(result -> result.getModelAndView().getView())
+                .andExpect(view().name(is(nullValue())))
                 .andDo(print());
     }
 }
